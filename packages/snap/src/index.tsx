ec02474cd2d6fc19361cfa034940b7ef25777790
Kanet <zombiedelah@gmail.com>
2024-08-13 15:13:28 +0900

    Example codes

--- packages/snap/src/index.tsx
+++ packages/snap/src/index.tsx
@@ -17,6 +17,9 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
 }) => {
   switch (request.method) {
     case 'hello':
+      const name: string = 'Radius';
+      const response = await getIndex(name);
+
       return snap.request({
         method: 'snap_dialog',
         params: {
@@ -33,6 +36,8 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
                 But you can edit the snap source code to make it do something,
                 if you want to!
               </Text>
+
+              <Text>Hello, {response['result']} from the sequencer!</Text>
             </Box>
           ),
         },
@@ -41,3 +46,23 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
       throw new Error('Method not found.');
   }
 };
+
+async function getIndex(name: &string) {
+  const response = await fetch('http://127.0.0.1:10000', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      'jsonrpc': '2.0',
+      'method': 'Index',
+      'params': [name],
+      'id': 1,
+    }),
+  })
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+  return await response.json();
+}
